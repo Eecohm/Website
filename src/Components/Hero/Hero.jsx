8e87291ee73ff16a2d111ca0667861abd89664d8
eecohm <eecohmplustwo@gmail.com>
2025-03-28 08:50:33 +0545

    color change

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
           EECOHM SCHOOL OF EXCELLENCE
         </h1>
         <h4 className="animate-text">
-          Learn . Grow . Innovate
+          LEARN . GROW . INNOVATE
         </h4>
         <p className="animate-text">
           Eecohm School of Excellence is a top-tier educational establishment that provides comprehensive education from Pre-school till High School Diploma. Our dynamic environment fosters intellectual, artistic, and physical growth in students, with an emphasis on academic excellence and skill-based education.
--- src/Components/Testemonials/Testomonial.jsx
+++ src/Components/Testemonials/Testomonial.jsx
@@ -6,14 +6,14 @@ const testimonialsData = [
   {
     photo: pranil,
     name: 'Pranil Chauhan',
-    description: 'Branch Executive @ Aussie Education Nepal',
+    description: 'CEO @ NEXT GEN LEARNERS',
     review: 'As a faculty member at EECOHM College, I am committed to bridging the gap between theoretical learning and real-world business applications. Through practical classes, I equip students with essential soft skills and provide hands-on entrepreneurial experiences that prepare them for the dynamic world of business. My goal is to nurture future leaders by fostering critical thinking, creativity, and a problem-solving mindset. At EECOHM, we don’t just teach business—we create opportunities for students to experience it firsthand.',
     stars: 5,
   },
   {
     photo: 'https://randomuser.me/api/portraits/women/44.jpg',
     name: 'Arpan Khatiwada',
-    description: 'Branch Executive @ BEES International Education Service',
+    description: 'Faculty of English',
     review: 'Having dedicated four years to teaching English at EECOHM School of Excellence, I can confidently say that it is a truly rewarding environment for both educators and students. The supportive management fosters open communication, ensuring that any concerns are promptly addressed, while the competitive remuneration reflects their commitment to valuing staff. This positive atmosphere directly enhances my teaching experience, allowing me to bring enthusiasm and energy into the classroom. When a teacher is fulfilled, that joy radiates to the students, sparking their curiosity and fostering a genuine love for learning. EECOHM is a place where excellence in education thrives—for teachers and students alike',
     stars: 4,
   },
@@ -58,6 +58,7 @@ const Testimonials = () => {
         <div className="testimonial-text-div">
           <h3 className="testimonial-name">{testimonialsData[currentIndex].name}</h3>
           <p className="testimonial-description">
+
             {testimonialsData[currentIndex].description}
           </p>
           <p className="testimonial-review">"{testimonialsData[currentIndex].review}"</p>
