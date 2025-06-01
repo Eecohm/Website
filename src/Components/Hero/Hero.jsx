eab1d36a22d6261f70173e8f4e84c0349fae19c3
BishamSinchiury <bishamsinchiury1116@gmail.com>
2025-06-01 09:21:57 +0545

    here

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -14,12 +14,12 @@ function Hero() {
         <p className="animate-text">
           Eecohm School of Excellence is a top-tier educational establishment that provides comprehensive education from Pre-school till High School Diploma. Our dynamic environment fosters intellectual, artistic, and physical growth in students, with an emphasis on academic excellence and skill-based education.
         </p>
-        <a href="http:/login">
+        <link to="/login">
         <button className="login-btn">Log In</button>
-        </a>
-        <a href="http:/register">
+        </link>
+        <link to="/register">
         <button className="register-btn">Register</button>
-        </a>
+        </link>
       </div>
     </div>
   );
