bffdcbd5a7f59a92facf68240828b9dd9ba0db3d
BishamSinchiury <bishamsinchiury1116@gmail.com>
2025-07-30 15:00:52 +0545

    fix profile import

--- src/Routs/PrivateRoutes.jsx
+++ src/Routs/PrivateRoutes.jsx
@@ -8,7 +8,7 @@ import StudentTable from "../Components/App/Students/Students";
 import Admin from "../Components/App/Admin/Admin";
 import ClassInfo from "../Components/App/Admin/ClassInfo/ClassInfo";
 import RegistrationApproval from "../Components/App/Admin/RegistrationApprovals/RegistartionApprovals";
-import Profile from "../Components/App/Profile/Profile";
+import Profile from "../Components/App/Profile/profile";
 import OrganizationForm from "../Components/App/Profile/OrganizationForm";
 import OrganizationData from "../Components/App/Profile/OrganizationData";
 import SubOrganizationForm from "../Components/App/Profile/SubOrganizationForm";
