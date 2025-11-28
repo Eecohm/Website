714e4201be55ce59f367a35542bff6f9fde91b97
BishamSinchiury <bishamsinchiury1116@gmail.com>
2025-11-28 07:48:01 +0545

    change url

--- src/MainJsx/App.jsx
+++ src/MainJsx/App.jsx
@@ -5,11 +5,11 @@ import { BaseUrlContext, BaseMediaUrlContext } from "@/Context/BaseUrlContext";
 import { AuthProvider } from "@/Context/AuthContext";
 
 const App = () => {
-  const baseUrl = "http://127.0.0.1:8000/api";
-  const basemediaUrl = "http://127.0.0.1:8000/media/";
+  // const baseUrl = "http://127.0.0.1:8000/api";
+  // const basemediaUrl = "http://127.0.0.1:8000/media/";
 
-  // const baseUrl = "https://bishamsinchiury.com.np/api";
-  // const basemediaUrl = "https://bishamsinchiury.com.np/media/";
+  const baseUrl = "https://bishamsinchiury.com.np/api";
+  const basemediaUrl = "https://bishamsinchiury.com.np/media/";
 
   return (
     <BaseUrlContext.Provider value={baseUrl}>
