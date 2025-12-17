6ab2c41bb8a64513c005a5b1bf956a44b913e54e
Bisham <bishamsinchiury1116@gmail.com>
2025-12-17 16:48:52 +0545

    ne

--- src/features/admin/Acadamic/ClassDetail/ClassDetail.jsx
+++ src/features/admin/Acadamic/ClassDetail/ClassDetail.jsx
@@ -1,8 +1,9 @@
 
 import NavBar from '@/features/admin/NavBar/NavBar';
+import { useState } from 'react';
 import styles from '@/features/admin/Acadamic/ClassDetail/ClassDetail.module.css';
-import { sync } from 'framer-motion';
 const ClassDetail = () => {
+    const [activeTab, setActiveTab] = useState('teachers');
     return (
         <div className={styles.pageContainer}>
             <NavBar />
@@ -10,11 +11,12 @@ const ClassDetail = () => {
                 <h2 className={styles.sectionHeading}>Class Management</h2>
                 <div className={styles.contentArea}>
                     <div className={styles.sections}>
-                        <h3 className={styles.sectionTabs}>Teachers and Attendence</h3>
-                        <h3 className={styles.sectionTabs}>Teachers and Attendence</h3>
-                        <h3 className={styles.sectionTabs}>Teachers and Attendence</h3>
+                        <h3 
+                        className={`${styles.sectionTabs} ${activeTab === 'teachers' ? styles.box : ''}`}
+                        onClick={() => setActiveTab('teachers')}>Teachers and Attendence</h3>
+                        <h3 className={`${styles.sectionTabs} ${activeTab === 'students' ? styles.box : ''}`} onClick={() => setActiveTab('students')}>Students</h3>
+                        <h3 className={`${styles.sectionTabs} ${activeTab === 'attendance' ? styles.box : ''}`} onClick={() => setActiveTab('attendance')}>Attendence and Notice</h3>
                     </div>
-
                 </div>
             </div>
 
